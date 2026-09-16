b578a3ab00905cacb9fd43466498dfba263916ca
aymen-kacem <aymenkacem2019@gmail.com>
2026-09-16 12:30:08 +0100

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-vue' // ou @vitejs/plugin-react selon votre projet
+import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
